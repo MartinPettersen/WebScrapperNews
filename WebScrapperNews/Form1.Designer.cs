20040adb4388dbac32a47622f8ce2e7fbeaae650
MartinPettersen <searchingtheskyfor@gmail.com>
2024-12-23 14:27:21 +0100

    uppdate design

--- WebScrapperNews/Form1.Designer.cs
+++ WebScrapperNews/Form1.Designer.cs
@@ -28,46 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.siteTextBox = new System.Windows.Forms.TextBox();
+            this.addSiteButton = new System.Windows.Forms.Button();
+            this.siteListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // siteTextBox
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(65, 74);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(783, 34);
-            this.textBox1.TabIndex = 0;
+            this.siteTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siteTextBox.Location = new System.Drawing.Point(65, 74);
+            this.siteTextBox.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.siteTextBox.Name = "siteTextBox";
+            this.siteTextBox.Size = new System.Drawing.Size(468, 34);
+            this.siteTextBox.TabIndex = 0;
             // 
-            // button1
+            // addSiteButton
             // 
-            this.button1.Location = new System.Drawing.Point(877, 74);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(298, 34);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.addSiteButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(115)))), ((int)(((byte)(22)))));
+            this.addSiteButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.addSiteButton.Location = new System.Drawing.Point(550, 68);
+            this.addSiteButton.Name = "addSiteButton";
+            this.addSiteButton.Size = new System.Drawing.Size(298, 46);
+            this.addSiteButton.TabIndex = 1;
+            this.addSiteButton.Text = "Legg til Side";
+            this.addSiteButton.UseVisualStyleBackColor = false;
+            this.addSiteButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // listBox1
+            // siteListBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 29;
-            this.listBox1.Location = new System.Drawing.Point(65, 126);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(783, 120);
-            this.listBox1.TabIndex = 2;
+            this.siteListBox.FormattingEnabled = true;
+            this.siteListBox.ItemHeight = 29;
+            this.siteListBox.Location = new System.Drawing.Point(65, 126);
+            this.siteListBox.Name = "siteListBox";
+            this.siteListBox.Size = new System.Drawing.Size(783, 207);
+            this.siteListBox.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1277, 941);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(37)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(919, 411);
+            this.Controls.Add(this.siteListBox);
+            this.Controls.Add(this.addSiteButton);
+            this.Controls.Add(this.siteTextBox);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form1";
@@ -79,9 +83,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox siteTextBox;
+        private System.Windows.Forms.Button addSiteButton;
+        private System.Windows.Forms.ListBox siteListBox;
     }
 }
 
